562376fb79ef8b519e6692018cb3457f14a5ef16
Jerin Philip <jerin.philip@research.iiit.ac.in>
2018-10-09 12:00:48 +0530

    Checkpointing progress

--- mgan/models/mgan/__init__.py
+++ mgan/models/mgan/__init__.py
@@ -15,6 +15,7 @@ import torch
 
 from .generator import MGANGenerator
 from .discriminator import MGANDiscriminator
+from .critic import MGANCritic
 
 class MaskGAN(nn.Module):
     """
@@ -30,13 +31,12 @@ class MaskGAN(nn.Module):
     def build_model(cls, args, task):
         generator = MGANGenerator.build_model(args, task)
         discriminator = MGANDiscriminator.build_model(args, task)
-        critic = None
+        critic = MGANCritic.build_model(args, task)
         return cls(generator, discriminator, critic)
 
+    """
     def forward(self, src_tokens, src_lengths, prev_output_tokens):
         logits, attns = self.generator(src_tokens, src_lengths, prev_output_tokens)
-
-
         bsz, seqlen, vocab_size = logits.size()
         # print("Logits size:", logits.size())
 
@@ -45,7 +45,7 @@ class MaskGAN(nn.Module):
         distribution = {}
         for t in range(seqlen):
             # input is B x T x C post transposing
-            logit = logits[:, t, :].detach()
+            logit = logits[:, t, :]
             # Good news, categorical works for a batch.
             # B x H dimension. Looks like logit's are already in that form.
             distribution[t] = Categorical(logits=logit)
@@ -80,5 +80,66 @@ class MaskGAN(nn.Module):
             E_R += R[t]*d.log_prob(samples[:, t])
 
         return -1*E_R
+    """
 
+
+
+def train(model, opt, src_token, src_lengths, prev_output_tokens):
+    logits, attns = model.generator(src_tokens, src_lengths, prev_output_tokens)
+    bsz, seqlen, vocab_size = logits.size()
+    # print("Logits size:", logits.size())
+
+    # Sample from x converting it to probabilities
+    samples = []
+    distribution = {}
+    for t in range(seqlen):
+        # input is B x T x C post transposing
+        logit = logits[:, t, :]
+        # Good news, categorical works for a batch.
+        # B x H dimension. Looks like logit's are already in that form.
+        distribution[t] = Categorical(logits=logit)
+
+        # Output is H dimension?
+        sampled = distribution[t].sample().unsqueeze(1)
+        samples.append(sampled)
+        
+
+    # Once all are sampled, it's possible to find the rewards from the generator.
+    samples = torch.cat(samples, dim=1)
+    # I may need to strip off an extra token generated.
+    samples = samples[:, 1:]
+    warn("Samples may not be the correct size. May need fixing")
+    # print("Samples:", samples.size(), "src_tokens_size:", src_tokens.size())
+    probs, attn_scores = model.discriminator(samples, src_lengths, prev_output_tokens)
+    r = []
+    for t in range(seqlen-1):
+        _r = torch.log(probs[:, t])
+        r.append(_r)
+
+    R = [0 for i in range(seqlen)]
+    gamma_0 = 0.95
+    gamma_t = gamma_0
+    for t in reversed(range(seqlen-1)):
+        R[t] = gamma_t * r[t] + R[t+1]
+        gamma_t = gamma_t*gamma_0
+
+    E_R = 0
+    for t in range(seqlen-1):
+        d = distribution[t]
+        E_R += R[t]*d.log_prob(samples[:, t])
+
+    gloss = -1*E_R
+
+    opt.zero_grad()
+    gloss.backward()
+    opt.step()
+
+
+    # expected_rewards, attn_scores = model.critic(samples, src_lengths, prev_output_tokens)
+    # loss = expected_rewards - torch.Tensor(
+
+
+    # need to create a mix of real and fake data.
+
+    dloss = 
 
---
+++ mgan/models/mgan/critic.py
@@ -0,0 +1,116 @@
+from fairseq.models.lstm \
+        import LSTMEncoder, \
+               LSTMDecoder, \
+               LSTMModel, \
+               base_architecture, \
+               Embedding
+
+from fairseq import options, utils
+
+from torch import nn
+import torch
+
+class MGANCriticEncoder(LSTMEncoder):
+    pass
+
+class MGANCriticDecoder(LSTMDecoder):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        out_embed_dim = self.additional_fc.out_features if hasattr(self, "additional_fc") else self.hidden_size
+        self.fc_out = nn.Linear(out_embed_dim, 1)
+
+    def forward(self, prev_output_tokens, encoder_out_dict, incremental_state=None):
+        x, attn_scores = super().forward(prev_output_tokens, encoder_out_dict, incremental_state)
+        # x = torch.sigmoid(x)
+        return x, attn_scores
+
+
+class MGANCritic(LSTMModel):
+    @classmethod
+    def build_model(cls, args, task):
+        """Build a new model instance."""
+        base_architecture(args)
+
+        def load_pretrained_embedding_from_file(embed_path, dictionary, embed_dim):
+            num_embeddings = len(dictionary)
+            padding_idx = dictionary.pad()
+            embed_tokens = Embedding(num_embeddings, embed_dim, padding_idx)
+            embed_dict = utils.parse_embedding(embed_path)
+            utils.print_embed_overlap(embed_dict, dictionary)
+            return utils.load_embedding(embed_dict, dictionary, embed_tokens)
+
+        if args.encoder_embed_path:
+            pretrained_encoder_embed = load_pretrained_embedding_from_file(
+                args.encoder_embed_path, task.source_dictionary, args.encoder_embed_dim)
+        else:
+            num_embeddings = len(task.source_dictionary)
+            pretrained_encoder_embed = Embedding(
+                num_embeddings, args.encoder_embed_dim, task.source_dictionary.pad()
+            )
+
+        if args.share_all_embeddings:
+            # double check all parameters combinations are valid
+            if task.source_dictionary != task.target_dictionary:
+                raise RuntimeError('--share-all-embeddings requires a joint dictionary')
+            if args.decoder_embed_path and (
+                    args.decoder_embed_path != args.encoder_embed_path):
+                raise RuntimeError(
+                    '--share-all-embed not compatible with --decoder-embed-path'
+                )
+            if args.encoder_embed_dim != args.decoder_embed_dim:
+                raise RuntimeError(
+                    '--share-all-embeddings requires --encoder-embed-dim to '
+                    'match --decoder-embed-dim'
+                )
+            pretrained_decoder_embed = pretrained_encoder_embed
+            args.share_decoder_input_output_embed = True
+        else:
+            # separate decoder input embeddings
+            pretrained_decoder_embed = None
+            if args.decoder_embed_path:
+                pretrained_decoder_embed = load_pretrained_embedding_from_file(
+                    args.decoder_embed_path,
+                    task.target_dictionary,
+                    args.decoder_embed_dim
+                )
+        # one last double check of parameter combinations
+        if args.share_decoder_input_output_embed and (
+                args.decoder_embed_dim != args.decoder_out_embed_dim):
+            raise RuntimeError(
+                '--share-decoder-input-output-embeddings requires '
+                '--decoder-embed-dim to match --decoder-out-embed-dim'
+            )
+
+        encoder = MGANCriticEncoder(
+            dictionary=task.source_dictionary,
+            embed_dim=args.encoder_embed_dim,
+            hidden_size=args.encoder_hidden_size,
+            num_layers=args.encoder_layers,
+            dropout_in=args.encoder_dropout_in,
+            dropout_out=args.encoder_dropout_out,
+            bidirectional=args.encoder_bidirectional,
+            pretrained_embed=pretrained_encoder_embed,
+        )
+        decoder = MGANCriticDecoder(
+            dictionary=task.target_dictionary,
+            embed_dim=args.decoder_embed_dim,
+            hidden_size=args.decoder_hidden_size,
+            out_embed_dim=args.decoder_out_embed_dim,
+            num_layers=args.decoder_layers,
+            dropout_in=args.decoder_dropout_in,
+            dropout_out=args.decoder_dropout_out,
+            attention=options.eval_bool(args.decoder_attention),
+            encoder_embed_dim=args.encoder_embed_dim,
+            encoder_output_units=encoder.output_units,
+            pretrained_embed=pretrained_decoder_embed,
+            share_input_output_embed=args.share_decoder_input_output_embed,
+            adaptive_softmax_cutoff=(
+                options.eval_str_list(args.adaptive_softmax_cutoff, type=int)
+                if args.criterion == 'adaptive_loss' else None
+            ),
+        )
+        return cls(encoder, decoder)
+
+    def forward(self, *args, **kwargs):
+        return super().forward(*args, **kwargs)
+
