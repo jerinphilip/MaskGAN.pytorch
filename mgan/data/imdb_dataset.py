Fixing masking; One error down; Going to train GANs.

mgan/data/imdb_dataset.py
@@ -81,12 +81,12 @@ class TensorIMDbDataset(IMDbDataset):
         
         idxs = []
         if move_eos_to_beginning:
-            mask = torch.zeros(token_count+2)
+            mask = torch.zeros(len(tokens)+2)
             idxs.append(self.vocab.eos())
             mask[1:token_count+1] = tmask[:token_count]
             token_count += 1
         else:
-            mask = torch.zeros(token_count+1)
+            mask = torch.zeros(len(tokens)+1)
             mask[:token_count] = tmask[:token_count]
 
         for token in tokens:
@@ -115,14 +115,14 @@ class TensorIMDbDataset(IMDbDataset):
         tgts = tgts.index_select(1, sort_order)
 
         # TODO(jerin): Fix this.
-        # src_masks = torch.stack(src_masks).permute(1, 0).contiguous()
-        # src_masks = src_masks.index_select(1, sort_order)
+        src_masks = torch.stack(src_masks).permute(1, 0).contiguous()
+        src_masks = src_masks.index_select(1, sort_order)
 
-        # tgt_masks = torch.stack(tgt_masks).permute(1, 0).contiguous()
-        # tgt_masks = tgt_masks.index_select(1, sort_order)
+        tgt_masks = torch.stack(tgt_masks).permute(1, 0).contiguous()
+        tgt_masks = tgt_masks.index_select(1, sort_order)
 
-        src_masks = None
-        tgt_masks = None
+        # src_masks = None
+        # tgt_masks = None
 
         batch_first = True
 

mgan/modules/distributed_train.py
@@ -10,12 +10,10 @@ class DistributedTrain:
         self.model = self.model.to(device)
         self.distributed_model = DataParallel(model)
         self.opt = opt
+        self.device = device
 
-    def load_state_dict(self, state):
-        self.model.load_state_dict(state)
-
-    def state_dict(self):
-        return self.model.state_dict()
+    def parallel(self):
+        self.model = self.model.to(self.device)
 
     def __call__(self, *args, **kwargs):
         self.opt.zero_grad()
@@ -27,6 +25,6 @@ class DistributedTrain:
 
     def eval(self, *args, **kwargs):
         with torch.no_grad():
-            loss, _ = self.model(*args, **kwargs)
+            loss, samples = self.model(*args, **kwargs)
             loss = loss.mean()
-            return loss.item()
+            return (loss.item(), samples)

mgan/sandbox.py
@@ -13,7 +13,6 @@ from torch.utils.data import DataLoader
 # FairSeq imports
 from fairseq.meters import AverageMeter
 from fairseq.progress_bar import tqdm_progress_bar
-from fairseq.sequence_generator import SequenceGenerator
 
 from mgan.preproc import Preprocess
 from mgan.data import IMDbDataset, TensorIMDbDataset
@@ -21,8 +20,8 @@ from mgan.data import IMDbDataset, TensorIMDbDataset
 # from mgan.models import train, pretrain
 from mgan.modules import build_trainer
 from mgan.utils import Saver
+from mgan.utils.debug_generate import debug_generate
 from mgan.report_hooks import visdom
-
 
 class Args: 
     criterion = 'dummy'
@@ -39,7 +38,7 @@ def dataset_test(args):
 
     preprocess = Preprocess(mask, tokenize)
     dataset = TensorIMDbDataset(args.path, preprocess, truncate=20)
-    loader = DataLoader(dataset, batch_size=160, 
+    loader = DataLoader(dataset, batch_size=20, 
             collate_fn=TensorIMDbDataset.collate, 
             shuffle=True, num_workers=16)
 
@@ -59,7 +58,7 @@ def dataset_test(args):
     checkpoint_path = "/scratch/jerin/mgan/"
     saver = Saver(checkpoint_path)
     trainer = build_trainer("MLE", args, task)
-    #trainer = build_trainer("MGAN", args, task)
+    trainer = build_trainer("MGAN", args, task)
 
     saver.load_trainer(trainer)
 
@@ -71,31 +70,16 @@ def dataset_test(args):
             count += 1
             src, tgt = src.to(device), tgt.to(device)
             summary = trainer(src, src_lens, src_mask, tgt, tgt_lens, tgt_mask)
+            # visdom.log('generator-loss-vs-steps', 'line', summary['Generator Loss'])
             visdom.log('generator-loss-vs-steps', 'line', summary['Generator Loss'])
+            visdom.log('discriminator-real-loss-vs-steps', 'line', summary['Discriminator Real Loss'])
+            visdom.log('discriminator-fake-loss-vs-steps', 'line', summary['Discriminator Fake Loss'])
 
         avg_loss = meters["loss"].avg
         meters['epoch'].update(avg_loss)
         visdom.log('avg-generator-loss-vs-epoch', 'line', avg_loss)
         saver.checkpoint_trainer(trainer)
-
-        seq_gen = SequenceGenerator([trainer.generator.model.model], 
-                dataset.vocab, beam_size=5)
-        #pbar = tqdm_progress_bar(loader, epoch=epoch)
-        for src, src_lens, _, tgt, tgt_lens, _ in loader:
-            src = src.to(device)
-            encoder_input = {"src_tokens": src, "src_lengths": src_lens}
-            samples = seq_gen.generate(encoder_input, maxlen=20)
-            for i, sample in enumerate(samples):
-               src_str = dataset.vocab.string(src[i, :])
-               tgt_str = dataset.vocab.string(tgt[i, :])
-               pred_str = dataset.vocab.string(sample[0]['tokens'])
-               closure = lambda s: visdom.log("gen-output", "text-append", s)
-               closure("> {}".format(src_str))
-               closure("< {}".format(pred_str))
-               closure("< {}".format(tgt_str))
-               closure("")
-            trainer.generator.model.model.train()
-            break
+        #debug_generate(trainer.generator.model.model, loader, dataset.vocab, visdom)
 
 if __name__ == '__main__':
     parser = ArgumentParser()