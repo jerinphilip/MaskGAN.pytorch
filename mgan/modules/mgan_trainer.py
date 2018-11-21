a094f4169a48fd2b2e783201b1228d1582031894
Jerin Philip <jerin.philip@research.iiit.ac.in>
2018-11-21 23:20:55 +0530

    Training D before G; Monte Carlo Rollouts named properly

--- mgan/modules/mgan_trainer.py
+++ mgan/modules/mgan_trainer.py
@@ -8,7 +8,6 @@ class MGANTrainer:
     def __init__(self, args, task, saver, logger):
         device = torch.device("cuda")
         self.pretrain = False
-        import random
         self._model = MGANModel.build_model(args, task, pretrain=self.pretrain)
         self.model = DataParallel(self._model)
         self.model = self.model.to(device)
@@ -19,17 +18,16 @@ class MGANTrainer:
         self.step = 0
         self.saver.load("mgan", self.model.module)
 
-
     def run(self, epoch, samples):
-        g_steps, d_steps = 50, 50
+        num_rollouts = 50
         self.lr_scheduler.step(epoch)
-        self.run_gsteps(g_steps, samples)
-        self.run_dsteps(d_steps, samples)
-        # self.enhance_critic(samples)
+        self.rollout_discriminator(num_rollouts=num_rollouts, samples=samples)
+        self.rollout_generator(num_rollouts=num_rollouts, samples=samples)
+        # self.rollout_critic(num_rollouts=num_rollouts, samples=samples)
         self.saver.checkpoint("mgan", self.model.module)
         self.step += 1
 
-    def run_dsteps(self, d_steps, samples):
+    def rollout_discriminator(self, num_rollouts, samples):
         src_tokens, src_lengths, src_mask, \
             tgt_tokens, tgt_lengths, tgt_mask = samples
 
@@ -38,7 +36,7 @@ class MGANTrainer:
         loss = 0
         self.opt.zero_grad()
 
-        for step in range(d_steps):
+        for rollout in range(num_rollouts):
             _d_real_loss, _ = self.model(prev_output_tokens[:, 1:], 
                     src_lengths, tgt_mask, prev_output_tokens, 
                     tag="d-step", real=True)
@@ -62,18 +60,17 @@ class MGANTrainer:
         loss.backward()
         self.opt.step()
 
-        self.logger.log("discriminator/real", self.step, d_real_loss/d_steps)
-        self.logger.log("discriminator/fake", self.step, d_fake_loss/d_steps)
-        self.logger.log("discriminator",      self.step, (d_fake_loss+d_real_loss)/(2*d_steps))
+        self.logger.log("discriminator/real", self.step, d_real_loss/num_rollouts)
+        self.logger.log("discriminator/fake", self.step, d_fake_loss/num_rollouts)
+        self.logger.log("discriminator",      self.step, (d_fake_loss+d_real_loss)/(2*num_rollouts))
 
-    def enhance_critic(self, samples):
+    def rollout_critic(self, num_rollouts, samples):
         src_tokens, src_lengths, src_mask, \
             tgt_tokens, tgt_lengths, tgt_mask = samples
-        max_steps = 4
         closs = 0
         self.opt.zero_grad()
 
-        for steps in range(max_steps):
+        for rollout in range(num_rollouts):
             if random.random() < 0.3:
                 src_mask = torch.ones_like(src_mask)
             _gloss, samples, _closs, _ = self.model(src_tokens, src_lengths, src_mask,
@@ -84,10 +81,10 @@ class MGANTrainer:
 
         loss.backward()
         self.opt.step()
-        self.logger.log("critic/pretrain", self.step, closs/steps)
+        self.logger.log("critic/pretrain", self.step, closs/num_rollouts)
 
     
-    def run_gsteps(self, g_steps, samples):
+    def rollout_generator(self, num_rollouts, samples):
         src_tokens, src_lengths, src_mask, \
             tgt_tokens, tgt_lengths, tgt_mask = samples
 
@@ -98,7 +95,7 @@ class MGANTrainer:
         rgloss = 0
         rcloss = 0
 
-        for step in range(g_steps):
+        for rollout in range(num_rollouts):
             self.opt.zero_grad()
             _gloss, samples, _closs, _avg_reward = self.model(src_tokens, src_lengths, src_mask,
                     prev_output_tokens, tag="g-step")
@@ -121,6 +118,6 @@ class MGANTrainer:
         rgloss.backward()
         self.opt.step()
 
-        self.logger.log("generator/advantage", self.step, gloss/g_steps)
+        self.logger.log("generator/advantage", self.step, gloss/num_rollouts)
         self.logger.log("generator/reward/token", self.step, avg_reward)
-        self.logger.log("critic/loss", self.step, closs/g_steps)
+        self.logger.log("critic/loss", self.step, closs/num_rollouts)
